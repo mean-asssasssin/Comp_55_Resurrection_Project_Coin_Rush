b15c197d3d8069f72cef4080e4ecb0a49d7b9f74
mean-asssasssin <channen.hill@yahoo.com>
2026-01-21 21:31:44 -0800

    Writing file testing

---
+++ src/FileTest.java
@@ -0,0 +1,19 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileTest {
+    public static void main(String[] args) {
+    	String filePath = "TestFile/level1.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write("First Line");
+            writer.newLine();
+            writer.write("Second Line");
+            writer.newLine();
+            writer.write("Third Line");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
